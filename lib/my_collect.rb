8b126a198f968228ec681599498407c7cdfe665d
communal-tutorial-3267 <jrodden1@gmail.com>
2019-05-30 21:14:28 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,12 @@
+require 'pry'
 def my_collect(words)
   if block_given?
     i = 0
     new_collection = []
     while i < words.length
-      yield words[i] {|i| i}
-      new_collection <<
+      yield words[i]
+      binding.pry
+
       i = i + 1
     end
   end
