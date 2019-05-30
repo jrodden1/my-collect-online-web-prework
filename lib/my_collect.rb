2ce66e5369ab7fb4abc252f3cd143bd9c7c3ead2
communal-tutorial-3267 <jrodden1@gmail.com>
2019-05-30 21:28:03 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,8 +3,8 @@ def my_collect(words)
     i = 0
     new_collection = []
     while i < words.length
-      new_collection << yield words[i]
-      
+      new_var = yield words[i]
+      new_collection << new_var
       i = i + 1
     end
   end
